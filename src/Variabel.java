a5a8dfa7516443e3e20888c8c4ba2d32f3698777
risa1006 <risa@unida.ac.id>
2021-11-20 11:15:15 +0700

    UtsAlpro

---
+++ src/Variabel.java
@@ -0,0 +1,12 @@
+
+public class Variabel {
+	public static void main(String[] args) {
+
+		System.out.println("Asalamualaikum");
+		System.out.println("Nama saya: Risa");
+		System.out.println("Saya lahir di tahun: 2003");
+		
+	}
+}
+
+
